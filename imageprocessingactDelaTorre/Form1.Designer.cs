1280d2806ae4641b08a4437bacc2979c4fa4cdb7
Shijuchi <shanedelatorre2412@gmail.com>
2023-11-27 23:41:50 +0800

    Add project files.

---
+++ imageprocessingactDelaTorre/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace imageprocessingactDelaTorre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.basicCopyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.basicCopyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.greyScaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorIversionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.histogramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sepiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dIP2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadbgToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subtractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.onCamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.offCamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(347, 254);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(394, 77);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(347, 254);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.basicCopyToolStripMenuItem,
+            this.dIP2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1160, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // basicCopyToolStripMenuItem
+            // 
+            this.basicCopyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.basicCopyToolStripMenuItem1,
+            this.greyScaleToolStripMenuItem,
+            this.colorIversionToolStripMenuItem,
+            this.histogramToolStripMenuItem,
+            this.sepiaToolStripMenuItem});
+            this.basicCopyToolStripMenuItem.Name = "basicCopyToolStripMenuItem";
+            this.basicCopyToolStripMenuItem.Size = new System.Drawing.Size(172, 24);
+            this.basicCopyToolStripMenuItem.Text = "Image Processing Tool";
+            this.basicCopyToolStripMenuItem.Click += new System.EventHandler(this.basicCopyToolStripMenuItem_Click);
+            // 
+            // basicCopyToolStripMenuItem1
+            // 
+            this.basicCopyToolStripMenuItem1.Name = "basicCopyToolStripMenuItem1";
+            this.basicCopyToolStripMenuItem1.Size = new System.Drawing.Size(191, 26);
+            this.basicCopyToolStripMenuItem1.Text = "Basic Copy";
+            this.basicCopyToolStripMenuItem1.Click += new System.EventHandler(this.basicCopyToolStripMenuItem1_Click);
+            // 
+            // greyScaleToolStripMenuItem
+            // 
+            this.greyScaleToolStripMenuItem.Name = "greyScaleToolStripMenuItem";
+            this.greyScaleToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
+            this.greyScaleToolStripMenuItem.Text = "GreyScale";
+            this.greyScaleToolStripMenuItem.Click += new System.EventHandler(this.greyScaleToolStripMenuItem_Click);
+            // 
+            // colorIversionToolStripMenuItem
+            // 
+            this.colorIversionToolStripMenuItem.Name = "colorIversionToolStripMenuItem";
+            this.colorIversionToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
+            this.colorIversionToolStripMenuItem.Text = "Color Inversion";
+            this.colorIversionToolStripMenuItem.Click += new System.EventHandler(this.colorIversionToolStripMenuItem_Click);
+            // 
+            // histogramToolStripMenuItem
+            // 
+            this.histogramToolStripMenuItem.Name = "histogramToolStripMenuItem";
+            this.histogramToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
+            this.histogramToolStripMenuItem.Text = "Histogram";
+            this.histogramToolStripMenuItem.Click += new System.EventHandler(this.histogramToolStripMenuItem_Click);
+            // 
+            // sepiaToolStripMenuItem
+            // 
+            this.sepiaToolStripMenuItem.Name = "sepiaToolStripMenuItem";
+            this.sepiaToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
+            this.sepiaToolStripMenuItem.Text = "Sepia";
+            this.sepiaToolStripMenuItem.Click += new System.EventHandler(this.sepiaToolStripMenuItem_Click);
+            // 
+            // dIP2ToolStripMenuItem
+            // 
+            this.dIP2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem,
+            this.loadbgToolStripMenuItem,
+            this.subtractToolStripMenuItem,
+            this.onCamToolStripMenuItem,
+            this.offCamToolStripMenuItem});
+            this.dIP2ToolStripMenuItem.Name = "dIP2ToolStripMenuItem";
+            this.dIP2ToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.dIP2ToolStripMenuItem.Text = "DIP 2";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
+            this.loadImageToolStripMenuItem.Text = "load image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // loadbgToolStripMenuItem
+            // 
+            this.loadbgToolStripMenuItem.Name = "loadbgToolStripMenuItem";
+            this.loadbgToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
+            this.loadbgToolStripMenuItem.Text = "loadbg";
+            this.loadbgToolStripMenuItem.Click += new System.EventHandler(this.loadbgToolStripMenuItem_Click);
+            // 
+            // subtractToolStripMenuItem
+            // 
+            this.subtractToolStripMenuItem.Name = "subtractToolStripMenuItem";
+            this.subtractToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
+            this.subtractToolStripMenuItem.Text = "subtract";
+            this.subtractToolStripMenuItem.Click += new System.EventHandler(this.subtractToolStripMenuItem_Click);
+            // 
+            // onCamToolStripMenuItem
+            // 
+            this.onCamToolStripMenuItem.Name = "onCamToolStripMenuItem";
+            this.onCamToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
+            this.onCamToolStripMenuItem.Text = "OnCam";
+            this.onCamToolStripMenuItem.Click += new System.EventHandler(this.onCamToolStripMenuItem_Click);
+            // 
+            // offCamToolStripMenuItem
+            // 
+            this.offCamToolStripMenuItem.Name = "offCamToolStripMenuItem";
+            this.offCamToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
+            this.offCamToolStripMenuItem.Text = "OffCam";
+            this.offCamToolStripMenuItem.Click += new System.EventHandler(this.offCamToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(785, 77);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(347, 254);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // openFileDialog3
+            // 
+            this.openFileDialog3.FileName = "openFileDialog3";
+            this.openFileDialog3.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog3_FileOk);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(409, 257);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 592);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem basicCopyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem basicCopyToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem greyScaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorIversionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem histogramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sepiaToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ToolStripMenuItem dIP2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadbgToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subtractToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
+        private System.Windows.Forms.ToolStripMenuItem onCamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem offCamToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ imageprocessingactDelaTorre/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebCamLib;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Diagnostics.Tracing;
+
+namespace imageprocessingactDelaTorre
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loaded;
+        Bitmap result;
+        Bitmap imageB, imageA, resultGreen;
+        Device camDevice;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+           
+        }
+       
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            loaded = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = loaded;
+        }
+
+
+        private void basicCopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void greyScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            result = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            for(int i = 0;i < loaded.Width;i++)
+            {
+                for (int j = 0; j < loaded.Height;j++)
+                {
+                    pixel= loaded.GetPixel(i,j);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    Color c = Color.FromArgb(grey, grey, grey);
+                    result.SetPixel(i, j, c);
+                }
+            }
+            pictureBox2.Image = result;
+        }
+
+        private void colorIversionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            result = new Bitmap(loaded.Width,loaded.Height);
+            Color pixel;
+            for (int i = 0; i < loaded.Width;i++)
+            {
+                for(int j = 0;j < loaded.Height;j++)
+                {
+                    pixel = loaded.GetPixel(i,j);
+                    Color c = Color.FromArgb(255 - pixel.R,255-pixel.G,255-pixel.B);
+                    result.SetPixel(i, j, c);
+                }
+            }
+            pictureBox2.Image = result;
+        }
+
+       
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            result.Save(saveFileDialog1.FileName);
+        }
+
+        private void basicCopyToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            result = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    pixel = loaded.GetPixel(i, j);
+                    result.SetPixel(i, j, pixel);
+                }
+            }
+            pictureBox2.Image = result;
+        }
+
+        private void histogramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //convert everything to greyscale
+            result = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    pixel = loaded.GetPixel(i, j);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    Color c = Color.FromArgb(grey, grey, grey);
+                    result.SetPixel(i, j, c);
+                }
+            }
+            //create histogram data array
+            int[] histData = new int[256];
+            Color hist;
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    hist = result.GetPixel(i, j);
+                    histData[hist.R]++;
+                }
+            }
+            //create bitmap for the graph
+            Bitmap diagram = new Bitmap(256,1000);
+            //create background for graph
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    diagram.SetPixel(i, j,Color.White);
+
+                }
+            }
+            //plot data
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < Math.Min(histData[i]/10,1000); j++)
+                {
+                   diagram.SetPixel(i,999-j,Color.Black);
+                }
+            }
+           
+            pictureBox2.Image = diagram;
+
+        }
+
+        private void sepiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            result = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            int r,g,b;
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    pixel = loaded.GetPixel(i, j);
+                    int tr = (int)(0.393 * pixel.R + 0.769 * pixel.G + 0.189 * pixel.B);
+                    int tg = (int)(0.349 * pixel.R + 0.686 * pixel.G + 0.168 * pixel.B);
+                    int tb = (int)(0.272 * pixel.R + 0.534 * pixel.G + 0.131 * pixel.B);
+
+                    if(tr > 255)
+                    {
+                        r = pixel.R;
+                    }
+                    else 
+                    {
+                        r = tr;
+                    }
+
+                    if (tg > 255)
+                    {
+                        g = pixel.G;
+                    }
+                    else
+                    {
+                        g = tg;
+                    }
+
+                    if (tb > 255)
+                    {
+                       b = pixel.B;
+                    }
+                    else
+                    {
+                        b = tb;
+                    }
+                    Color color = Color.FromArgb(r,g,b);
+                    result.SetPixel(i, j, color);
+                }
+            }
+            pictureBox2.Image = result;
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void brightnessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void contrastToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadbgToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog3.ShowDialog();
+        }
+
+        private void openFileDialog3_FileOk(object sender, CancelEventArgs e)
+        {
+            imageA = new Bitmap(openFileDialog3.FileName);
+            pictureBox2.Image = imageA;
+        }
+
+        private void subtractToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //imageA is the bg of imageB
+            Color g = Color.FromArgb(0, 0, 255);
+            int greyG = (g.R+g.G+g.B)/3;
+            int threshold = 10;
+            resultGreen = new Bitmap (imageB.Width, imageB.Height);
+            
+
+            for(int i = 0; i < imageB.Width; i++)
+            {
+                for(int j = 0; j < imageB.Height; j++)
+                {
+                    Color pixel = imageB.GetPixel(i, j);
+                    Color bgPixel = imageA.GetPixel(i, j);
+                    int grey = (pixel.R + pixel.G + pixel.B)/3;
+                    int subtractValue = Math.Abs(grey - greyG);
+                    if(subtractValue < threshold)
+                    {
+                        resultGreen.SetPixel(i, j, bgPixel);
+                    }
+                    else
+                    {
+                        resultGreen.SetPixel(i, j, pixel);
+                    }
+                }
+            }
+            pictureBox3.Image = resultGreen;
+        }
+
+        private void onCamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Device[] devices = DeviceManager.GetAllDevices();
+
+            if (devices.Length > 0)
+            {
+                camDevice = devices[0]; // Assuming you want to use the first detected device
+                camDevice.ShowWindow(pictureBox1);
+            }
+            else
+            {
+                MessageBox.Show("No webcam devices found.");
+            }
+        }
+
+        private void offCamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            camDevice.Stop();
+
+        }
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            imageB = new Bitmap(openFileDialog2.FileName);
+            pictureBox1.Image = imageB;
+        }
+
+        /*
+        public void brightness(ref Bitmap a, ref Bitmap b)
+        {
+
+        }
+
+        public static void Equalisation() 
+        {
+
+        }
+        */
+
+
+    }
+}
